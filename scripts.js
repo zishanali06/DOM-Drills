c28108f9f8b627abf818e6fbe1c07ebcf00d92b1
Zishan Ali <zishanali06@gmail.com>
2019-02-25 13:45:23 -0600

    added h3-h6

--- scripts.js
+++ scripts.js
@@ -19,6 +19,28 @@ document.addEventListener('DOMContentLoaded', function(){
     newH2.appendChild(h2Text);
     newDiv.appendChild(newH2);
 
-    
+    //create a h3 element with text and add to head-container div
+    let newH3 = document.createElement('h3');
+    let h3Text = document.createTextNode('This is an h3');
+    newH3.appendChild(h3Text);
+    newDiv.appendChild(newH3);
+
+    //create a h4 element with text and add to head-container div
+    let newH4 = document.createElement('h4');
+    let h4Text = document.createTextNode('This is an h4');
+    newH4.appendChild(h4Text);
+    newDiv.appendChild(newH4);
+
+    //create a h5 element with text and add to head-container div
+    let newH5 = document.createElement('h5');
+    let h5Text = document.createTextNode('This is an h5');
+    newH5.appendChild(h5Text);
+    newDiv.appendChild(newH5);
+
+    //create a h6 element with text and add to head-container div
+    let newH6 = document.createElement('h6');
+    let h6Text = document.createTextNode('This is an h6');
+    newH6.appendChild(h6Text);
+    newDiv.appendChild(newH6);
 });
 
