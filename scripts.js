037a5acb2f792655d8ea80311b3160e74cd82954
Zishan Ali <zishanali06@gmail.com>
2019-02-25 13:42:31 -0600

    added h2 and readme.md, added comments as well

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
+//add listender to document to listen for DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function(){
+    //create new div
     let newDiv = document.createElement('div');
     newDiv.className = 'header-container';
     
+    //create new h1
     let newH1 = document.createElement('h1');
-    let h1Content = document.createTextNode('This is an h1');
+    let h1Text = document.createTextNode('This is an h1');
     
-    newH1.appendChild(h1Content);
+    //place h1 in div and div inside the body
+    newH1.appendChild(h1Text);
     newDiv.appendChild(newH1);
     document.body.appendChild(newDiv);
+
+    //create a h2 element with text and add to head-container div
+    let newH2 = document.createElement('h2');
+    let h2Text = document.createTextNode('This is an h2');
+    newH2.appendChild(h2Text);
+    newDiv.appendChild(newH2);
+
+    
 });
 
